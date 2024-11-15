e3c0eccc42f335d56c3aa88f795915b3643dbdf1
Satish9047 <satishprajapati930@gmail.com>
2024-11-15 10:05:48 +0545

    00: Project Initialization + setup Folder structure + setup Environment

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+  constructor(
+    id: string,
+    username: string,
+    email: string,
+    phone: string,
+    address: string,
+    isValid: boolean,
+    isAdmin: boolean,
+  ) {}
+}
---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
+}
