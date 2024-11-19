6703ba82a5c45799cfdd790a9f3a66d633cb0caa
Satish9047 <satishprajapati930@gmail.com>
2024-11-19 21:57:08 +0545

    25: Add Admin RequestedOrder Component

---
+++ src/app/features/admin/requested-orders/requested-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requested-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './requested-orders.component.html',
+  styleUrl: './requested-orders.component.css'
+})
+export class RequestedOrdersComponent {
+
+}
