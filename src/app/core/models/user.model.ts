68dfec36308fead94b9f7eb7ed0ba35d37641581
Satish9047 <satishprajapati930@gmail.com>
2024-11-20 20:59:47 +0545

    34: Update User Model

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -1,11 +1,14 @@
 export class User {
   constructor(
-    id: string,
-    username: string,
-    email: string,
-    phone: string,
-    address: string,
-    isValid: boolean,
-    isAdmin: boolean,
+    public _id: string,
+    public name: string,
+    public email: string,
+    public phone: string,
+    public address: string,
+    public isVerified: boolean,
+    public role: string,
+    public createdAt: string,
+    public updatedAt: string,
+    public __v: number,
   ) {}
 }
