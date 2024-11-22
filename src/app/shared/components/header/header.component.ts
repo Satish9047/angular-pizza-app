84e8e65eba0af1c4972334432a7ca1e940f700d7
Satish9047 <satishprajapati930@gmail.com>
2024-11-22 21:49:23 +0545

    44: Add OnInit lifeCycle

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,24 +1,39 @@
 import { AuthService } from './../../../core/services/auth.service';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { NgIf } from '@angular/common';
 import {
   Router,
   RouterLink,
   RouterLinkActive,
   RouterOutlet,
 } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AsyncPipe, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isAuth: boolean = false;
+  user$!: Observable<User | null>;
   constructor(
     private authService: AuthService,
     private router: Router,
   ) {}
+
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.isAuth = true;
+    }
+
+    this.user$ = this.authService.user$;
+  }
 
   logout() {
     this.authService.logout();
