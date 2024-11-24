74e649963d8e33241d50cb181d34c4194c4ba3b6
Satish9047 <satishprajapati930@gmail.com>
2024-11-24 22:39:51 +0545

    48: Update Import proper sequence

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
+
+import { authGuard } from './core/guard/auth.guard';
 import { LoginComponent } from './features/auth/login/login.component';
+import { ProfileComponent } from './features/users/profile/profile.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { DashboardComponent } from './features/users/dashboard/dashboard.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { ProfileComponent } from './features/users/profile/profile.component';
-import { CustomOrderComponent } from './features/users/custom-order/custom-order.component';
-import { YourOrdersComponent } from './features/users/your-orders/your-orders.component';
 import { InventoryComponent } from './features/admin/inventory/inventory.component';
+import { YourOrdersComponent } from './features/users/your-orders/your-orders.component';
+import { CustomOrderComponent } from './features/users/custom-order/custom-order.component';
 import { RequestedOrdersComponent } from './features/admin/requested-orders/requested-orders.component';
-import { authGuard } from './core/guard/auth.guard';
 
 export const routes: Routes = [
   {
