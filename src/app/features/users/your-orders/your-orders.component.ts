49ef3845bae4e9eacb0a9edf8fd4917e6fbf6db7
Satish9047 <satishprajapati930@gmail.com>
2024-11-24 22:39:07 +0545

    47: Add RefreshToken Interceptor

--- src/app/features/users/your-orders/your-orders.component.ts
+++ src/app/features/users/your-orders/your-orders.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Orders } from '../../../core/interfaces/order';
+import { OrderService } from '../../../core/services/order.service';
 
 @Component({
   selector: 'app-your-orders',
   standalone: true,
   imports: [],
   templateUrl: './your-orders.component.html',
-  styleUrl: './your-orders.component.css'
+  styleUrl: './your-orders.component.css',
 })
-export class YourOrdersComponent {
+export class YourOrdersComponent implements OnInit {
+  yourOrders: Orders[] | null = [];
+  constructor(private orderService: OrderService) {}
 
+  ngOnInit(): void {
+    this.orderService.getUserOrders().subscribe((response) => {
+      this.yourOrders = response.data;
+      console.log(response);
+    });
+  }
 }
