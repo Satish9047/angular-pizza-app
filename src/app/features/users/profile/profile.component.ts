574d98a9313c2434039cbdf5db6e44ac0436f641
Satish9047 <satishprajapati930@gmail.com>
2024-11-25 21:36:59 +0545

    64: User Behavior Subject to get the User so it don't make request to api every time visit to profile page

--- src/app/features/users/profile/profile.component.ts
+++ src/app/features/users/profile/profile.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 
 import { AuthService } from '../../../core/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, AsyncPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent implements OnInit {
-  userInfo: any;
+  user$!: Observable<User | null>;
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.authService.me().subscribe((res) => {
-      console.log(res.data);
-      this.userInfo = res.data;
-    });
+    this.user$ = this.authService.user$;
   }
 }
